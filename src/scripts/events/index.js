f04f7c77aba4b6f504515123cb31f508fb649e8e
Fatih Kaya <fatihkaya13@gmail.com>
2021-12-15 20:29:32 +0300

    event for nodemailer

---
+++ src/scripts/events/index.js
@@ -0,0 +1,25 @@
+const eventEmitter = require("./eventEmitter");
+const nodemailer = require("nodemailer");
+module.exports = () => {
+    eventEmitter.on("send_email", async (emailData) => {
+
+        let transporter = nodemailer.createTransport({
+            host: process.env.EMAIL_HOST,
+            port: process.env.EMAIL_PORT,
+            secure: false, // true for 465, false for other ports
+            auth: {
+              user: process.env.EMAIL_USER, // generated ethereal user
+              pass: process.env.EMAIL_PASSWORD, // generated ethereal password
+            },
+          });
+        // send mail with defined transport object
+        let info =  await transporter.sendMail({
+            from: process.env.EMAIL_FROM, // sender address
+            ...emailData,
+        }, (err, info) => {
+            console.log(info.envelope);
+            console.log(info.messageId);
+            console.log(err)
+        });
+    })
+}
