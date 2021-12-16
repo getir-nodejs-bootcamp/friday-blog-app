be0a7795fa804a701f6959dcd377c56101d7156c
Fatih Kaya <fatihkaya13@gmail.com>
2021-12-16 16:21:38 +0300

    add send flag validation to validations

--- src/validations/Blogs.js
+++ src/validations/Blogs.js
@@ -3,16 +3,23 @@ const Joi = require("joi");
 const createValidation = Joi.object({
     text: Joi.string().required().min(5),
     title: Joi.string().required(),
-    category: Joi.string().required()
+    category: Joi.string().required(),
+    published: Joi.boolean()
 });
 
 const updateValidation = Joi.object({
     text: Joi.string().required().min(5),
     title: Joi.string().required(),
-    category: Joi.string().required
+    category: Joi.string(),
+    published: Joi.boolean()
+});
+
+const sendLikeFlagValidation = Joi.object({
+    liked: Joi.boolean()
 });
 
 module.exports = {
     createValidation,
-    updateValidation
+    updateValidation,
+    sendLikeFlagValidation
 }
