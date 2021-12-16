46b3c04cf1ca9939043b6c3fe2fe4a3e7a0ff5ed
Fatih Kaya <fatihkaya13@gmail.com>
2021-12-16 11:43:35 +0300

    model validation logic is added

--- src/validations/Blogs.js
+++ src/validations/Blogs.js
@@ -3,14 +3,12 @@ const Joi = require("joi");
 const createValidation = Joi.object({
     text: Joi.string().required().min(5),
     title: Joi.string().required(),
-    author: Joi.string().required(),
-    category: Joi.string().required
+    category: Joi.string().required()
 });
 
 const updateValidation = Joi.object({
     text: Joi.string().required().min(5),
     title: Joi.string().required(),
-    author: Joi.string().required(),
     category: Joi.string().required
 });
 
