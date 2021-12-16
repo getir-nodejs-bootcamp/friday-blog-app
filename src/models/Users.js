1152a378055cd0ddf812787c3a4f7bf02c1df940
Fatih Kaya <fatihkaya13@gmail.com>
2021-12-16 12:09:55 +0300

    add preferences field to User model and implement validationso

--- src/models/Users.js
+++ src/models/Users.js
@@ -4,7 +4,17 @@ const UserSchema = new Mongoose.Schema({
     full_name: String,
     password: String,
     email: String,
-    profile_image: String
+    profile_image: String,
+    preferences: {
+        sendMail: {
+            type: Boolean,
+            default: true
+        },
+        sendSMS: {
+            type: Boolean,
+            default: true
+        }
+    }
 }, {timestamps: true, versionKey: false})
 
 module.exports = Mongoose.model("user", UserSchema);
--- src/validations/Users.js
+++ src/validations/Users.js
@@ -3,7 +3,8 @@ const Joi = require("joi");
 const createValidation = Joi.object({
     full_name: Joi.string().required().min(3),
     password: Joi.string().required().min(8),
-    email: Joi.string().email().required().min(8)
+    email: Joi.string().email().required().min(8),
+    //preferences are default true and created at model level
 });
 
 const loginValidation = Joi.object({
@@ -21,7 +22,11 @@ const changePasswordValidation = Joi.object({
 
 const updateValidation = Joi.object({
     full_name: Joi.string().min(3),
-    email: Joi.string().email().min(8)
+    email: Joi.string().email().min(8),
+    preferences: Joi.object().keys({
+        sendMail: Joi.boolean(),
+        sendSMS: Joi.boolean()
+    })
 });
 
 module.exports = {
