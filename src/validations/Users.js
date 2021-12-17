7ceed25103f61102b5529a967eaeddc796c1a6fc
Fatih Kaya <fatihkaya13@gmail.com>
2021-12-17 13:06:01 +0300

    regex for phone number validation

--- src/validations/Users.js
+++ src/validations/Users.js
@@ -23,6 +23,13 @@ const changePasswordValidation = Joi.object({
 const updateValidation = Joi.object({
     full_name: Joi.string().min(3),
     email: Joi.string().email().min(8),
+    /**
+     *  the first letter for country code should start with 1 to 9
+     *  following this there might be additional numbers from 0 to 9 for up to 2 letters, optional
+     *  e.g the U.S case: number starts with 1 and goes with 10 digits
+     *  e.g the Turkey case: number starts with 90 and goes with 10 digits
+     */ 
+    phoneNumber: Joi.string().regex(/^[1-9]{1}(d{2})?\d{10}/).min(11).max(13),
     preferences: Joi.object().keys({
         sendMail: Joi.boolean(),
         sendSMS: Joi.boolean(),
